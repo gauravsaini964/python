7cc7cc06b6443e07f7da2e19947ca626498c0e5a
gauravsaini964 <gauravsaini964@gmail.com>
2017-06-12 22:50:25 +0530

    remove common element from string

--- subtractgame/removecommon.py
+++ subtractgame/removecommon.py
@@ -1,11 +1,53 @@
-#remove 1 fromm adjacent common element string
-# solve using stack
+#function to display stack
+def DisplayStack():
+    print("Current Items in stack")
+    for item in stack:
+        print(item)
+#function to push element into stack
+def Push(value):
+    if len(stack) < StackSize:
+        stack.append(value)
+    else:
+        print("Stack is full")
+#function to pop element
+def Pop(value):
+    if len(stack) > 0:
+        stack.pop(value)
+    else:
+        print("Stack is already empty")
 
 
-s = 'aabbaabbdd'
-len=strlen(s)
-i=0
-int=int(i)
 
-for i <= len:
+s = "aaabbaaaddcaacc"
+len1 = len(s)
+stack = []
+StackSize = len1/2
+
+
+if len(stack) <= 0:
+     for i in range(0,len1-1):
+         if s[i] == s[i+1]:
+             if s[i] not in stack:
+                 Push(s[i])
+                 print  "Element added: " +s[i]
+             else:
+                 print "Element already exist: " +s[i]
+
+DisplayStack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ subtractgame/test.py
@@ -0,0 +1,10 @@
+def func2():
+    ch1 = input("Enter one character. ")
+    str1 = input("Enter a string. ")
+    a = 0
+    for c in str1:
+        if c == ch:
+            a += 1
+        print(a)
+    return a
+
