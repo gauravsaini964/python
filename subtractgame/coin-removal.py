9b03b27ced24bed577741ea926fdfcee1a9e3783
gauravsaini964 <gauravsaini964@gmail.com>
2017-06-12 23:59:38 +0530

    Further reduced no. of loops

--- subtractgame/coin-removal.py
+++ subtractgame/coin-removal.py
@@ -1,5 +1,5 @@
-a=raw_input("enter no of coin")
-
-
-
-
+N = int(raw_input().strip())
+if N%2!=0 or (N%2==0 and 6<= N <=20):
+    print"Wierd"
+else:
+    print"Not Wierd"
--- subtractgame/odd-even--weird-notweird.py
+++ subtractgame/odd-even--weird-notweird.py
@@ -8,10 +8,8 @@ n=int(n)
 if n == 0:
     print("Enter any other value greater than 0..Pss 0 is EVEN")
 elif n % 2 == 0:
-    if n > 20:
-        print ("Greater than 20 | EVEN | Not weird")
-    elif n == 2 or n == 4:
-        print ("Between 2 & 5 | EVEN | Not weird")
+    if n == 2 or n == 4 or n > 20:
+        print ("Between 2 & 5 or greater than 20 | EVEN | Not weird")
     elif 6 <= n <= 20:
         print ("Between 6 & 20 | EVEN |  Weird")
 
