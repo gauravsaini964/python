b5071c30df8a7311ca3f4637c7a04895d39deff8
gauravsaini964 <gauravsaini964@gmail.com>
2017-06-12 23:49:50 +0530

    Odd-Even weird/Notweird--Stupidity correction

--- subtractgame/odd-even--weird-notweird.py
+++ subtractgame/odd-even--weird-notweird.py
@@ -7,13 +7,14 @@ n=int(n)
 
 if n == 0:
     print("Enter any other value greater than 0..Pss 0 is EVEN")
-elif n%2 == 0:
-    if n>20:
+elif n % 2 == 0:
+    if n > 20:
         print ("Greater than 20 | EVEN | Not weird")
-    elif n>=6<=20:
-        print ("Between 6 & 20 | EVEN |  Weird")
-    elif n>=2<6 :
+    elif n == 2 or n == 4:
         print ("Between 2 & 5 | EVEN | Not weird")
+    elif 6 <= n <= 20:
+        print ("Between 6 & 20 | EVEN |  Weird")
+
 else:
     print ("Oddly weird")
 
