9f86178d767b99a003960d8c4108f7a4bc4f7bde
gauravsaini964 <gauravsaini964@gmail.com>
2017-06-12 23:33:59 +0530

    Odd-Even weird/Notweird--Better Code

--- subtractgame/odd-even--weird-notweird.py
+++ subtractgame/odd-even--weird-notweird.py
@@ -5,14 +5,15 @@ import random
 n=raw_input("enter any no")
 n=int(n)
 
-if n%2 != 0:
-    print ("Oddly Weird")
-elif n == 0:
-    print("Enter any other value greater than 0")
-elif n>20 and n%2 ==0:
-    print ("Greater than 20 vala even Not weird")
-elif n>=6<=20 and n%2 == 0:
-    print ("6 se 20 ke beech wala even Weird")
-elif n>=2<6 and n%2 == 0:
-    print ("or 6 se neeche vala even Not weird")
+if n == 0:
+    print("Enter any other value greater than 0..Pss 0 is EVEN")
+elif n%2 == 0:
+    if n>20:
+        print ("Greater than 20 | EVEN | Not weird")
+    elif n>=6<=20:
+        print ("Between 6 & 20 | EVEN |  Weird")
+    elif n>=2<6 :
+        print ("Between 2 & 5 | EVEN | Not weird")
+else:
+    print ("Oddly weird")
 
