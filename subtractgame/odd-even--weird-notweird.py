c6c9b7b42bbb3200425cbdfeedc7300db6ce72d2
gauravsaini964 <gauravsaini964@gmail.com>
2017-06-13 00:11:20 +0530

    odd-even-weird-notweird-Fix yet another derp

--- subtractgame/odd-even--weird-notweird.py
+++ subtractgame/odd-even--weird-notweird.py
@@ -8,7 +8,7 @@ n=int(n)
 if n == 0:
     print("Enter any other value greater than 0..Pss 0 is EVEN")
 elif n % 2 == 0:
-    if n == 2 or n == 4 or n > 20:
+    if 2 <= n <= 5 or n > 20:
         print ("Between 2 & 5 or greater than 20 | EVEN | Not weird")
     elif 6 <= n <= 20:
         print ("Between 6 & 20 | EVEN |  Weird")
